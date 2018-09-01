6686b8571dc0332539cc8f7a561585f2490a49a7
nicosp <nicolassanpedro@gmail.com>
2018-09-01 15:39:43 -0300

    1st commit

---
+++ client/components/Single.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+const Single = React.createClass({
+    render() {
+      return(
+          <div className="single-photo">
+            i'm the single grid!
+          </div>
+      )
+    }
+});
+
+
+export default Single;
---
+++ client/store.js
@@ -0,0 +1,24 @@
+import { creteStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// Root reducer imports
+import rootReducer from './reducers/index';
+
+// Data imports
+import comments from './data/comments';
+import posts from './data/posts';
+
+
+// Default data obj
+
+const defaultState = {
+  posts,
+  comments
+};
+
+const store = creteStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
