5d0c53adcbdbedc3bf675051cc09c92185057d29
nicosp <nicolassanpedro@gmail.com>
2018-09-02 23:07:43 -0300

    displaying redux state inside components

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
-
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render() {
       return(
           <div className="photo-grid">
-            I'm the photo grid
+            {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post}/>)}
           </div>
       )
     }
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,7 @@
 function posts(state = [], action) {
+  console.log('POST IS GOING TO CHANGE!!');
   console.log(state, action);
   return state;
 }
 
-export default posts; 
+export default posts;
