609550fa9b1e3f8db41e9161b34bdd66c6d2ea0b
nicosp <nicolassanpedro@gmail.com>
2018-09-12 22:05:55 -0300

    Redux reducer composition

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,38 @@
+function postComments(state = [], action) {
+  // console.log(state, action);
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+
+      }]
+    case 'REMOVE_COMMENT':
+    console.log('removing comment');
+
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1)
+        ];
+
+    default:
+      return state;
+
+  }
+  return state;
+}
+
 function comments(state = [], action) {
   // console.log(state, action);
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // current state
+      ...state,
+      // overwrite
+      [action.postId]: postComments(state[action.postId], action)
+    }
+
+  }
   return state;
 }
 
